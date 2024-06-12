62d3ebd7d1e42815ac3dd09d1197e362a9e18a2b
akbarmansyur <nuryahakbar@gmail.com>
2024-06-12 14:24:52 +0800

    asd

--- lib/ble_controller.dart
+++ lib/ble_controller.dart
@@ -1,3 +1,87 @@
+// import 'package:flutter_blue/flutter_blue.dart';
+// import 'package:get/get.dart';
+// import 'package:permission_handler/permission_handler.dart';
+
+// class BleController extends GetxController {
+//   FlutterBlue ble = FlutterBlue.instance;
+//   var isScanning = false.obs;
+//   var connectedDevice = Rx<BluetoothDevice?>(null);
+//   final isConnecting = false.obs;
+//   final service = <BluetoothService>[].obs;
+//   var rpm = 0.obs;
+//   var response = ''.obs;
+//   BluetoothCharacteristic? _writeCharacteristic;
+//   BluetoothCharacteristic? _notifyCharacteristic;
+
+//   Future scanDevice() async {
+//     if (await Permission.bluetoothScan.request().isGranted) {
+//       if (await Permission.bluetoothConnect.request().isGranted) {
+//         isScanning(true);
+//         ble.startScan(
+//           timeout: const Duration(seconds: 5),
+//         );
+//         await Future.delayed(const Duration(seconds: 5));
+//         ble.stopScan();
+//         isScanning(false);
+//       }
+//     }
+//   }
+
+//   Stream<List<ScanResult>> get scanResult => ble.scanResults;
+
+//   Future<void> connectToDevice(BluetoothDevice device) async {
+//     isConnecting(true);
+//     await device.connect();
+//     connectedDevice.value = device;
+//     isConnecting(false);
+//     List<BluetoothService> discoveredServices = await device.discoverServices();
+//     service.value = discoveredServices;
+//     // Find the correct characteristics
+//     for (var serv in service) {
+//       for (var characteristic in serv.characteristics) {
+//         if (characteristic.properties.write) {
+//           _writeCharacteristic = characteristic;
+//         }
+//         if (characteristic.properties.notify) {
+//           _notifyCharacteristic = characteristic;
+//           await _notifyCharacteristic!.setNotifyValue(true);
+//           _notifyCharacteristic!.value.listen((value) {
+//             if (value.length >= 4) {
+//               // Parsing RPM data
+//               int rpmValue = ((value[2] * 256) + value[3]) ~/ 4;
+//               rpm.value = rpmValue;
+//             }
+//           });
+//         }
+//       }
+//       sendRPMCommand();
+//     }
+//   }
+
+// void sendRPMCommand() async {
+//   if (_writeCharacteristic != null) {
+//     // Send the PID command for RPM
+//     List<int> rpmCommand = [0x01, 0x0C];
+//     await _writeCharacteristic!.write(rpmCommand, withoutResponse: true);
+//   }
+// }
+
+//   void sendRPMCommandManually() async {
+//     if (_writeCharacteristic != null) {
+//       // Send the PID command for RPM
+//       List<int> rpmCommand = [0x01, 0x0C];
+//       print("Sending RPM command: $rpmCommand");
+//       await _writeCharacteristic!.write(rpmCommand, withoutResponse: false);
+
+//       // Listen for response
+//       await Future.delayed(Duration(seconds: 2)); // Adjust delay as needed
+//       print("Received response: ${response.value}");
+//     } else {
+//       print("Write characteristic not found");
+//     }
+//   }
+// }
+
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -6,20 +90,23 @@ class BleController extends GetxController {
   FlutterBlue ble = FlutterBlue.instance;
   var isScanning = false.obs;
   var connectedDevice = Rx<BluetoothDevice?>(null);
-  final isLoading = false.obs;
+  final isConnecting = false.obs;
   final service = <BluetoothService>[].obs;
   var rpm = 0.obs;
   var response = ''.obs;
-  BluetoothCharacteristic? _writeCharacteristic;
   BluetoothCharacteristic? _notifyCharacteristic;
+
+  final String targetServiceUUID =
+      "0000ffff-0000-1000-8000-00805f9b34fb"; // UUID layanan
+  final String notifyCharacteristicUUID =
+      "0000fff2-0000-1000-8000-00805f9b34fb"; // UUID karakteristik notify
 
   Future scanDevice() async {
     if (await Permission.bluetoothScan.request().isGranted) {
       if (await Permission.bluetoothConnect.request().isGranted) {
         isScanning(true);
-        ble.startScan(
-          timeout: const Duration(seconds: 10),
-        );
+        ble.startScan(timeout: const Duration(seconds: 10));
+        await Future.delayed(Duration(seconds: 10));
         ble.stopScan();
         isScanning(false);
       }
@@ -29,54 +116,37 @@ class BleController extends GetxController {
   Stream<List<ScanResult>> get scanResult => ble.scanResults;
 
   Future<void> connectToDevice(BluetoothDevice device) async {
-    isLoading(true);
+    isConnecting(true);
     await device.connect();
     connectedDevice.value = device;
-    isLoading(false);
+    isConnecting(false);
+
     List<BluetoothService> discoveredServices = await device.discoverServices();
     service.value = discoveredServices;
-    // Find the correct characteristics
+
     for (var serv in service) {
-      for (var characteristic in serv.characteristics) {
-        if (characteristic.properties.write) {
-          _writeCharacteristic = characteristic;
-        }
-        if (characteristic.properties.notify) {
-          _notifyCharacteristic = characteristic;
-          await _notifyCharacteristic!.setNotifyValue(true);
-          _notifyCharacteristic!.value.listen((value) {
-            if (value.length >= 4) {
-              // Parsing RPM data
-              int rpmValue = ((value[2] * 256) + value[3]) ~/ 4;
-              rpm.value = rpmValue;
-            }
-          });
+      if (serv.uuid.toString() == targetServiceUUID) {
+        print('Target Service Found: ${serv.uuid}');
+        for (var characteristic in serv.characteristics) {
+          print('Characteristic UUID: ${characteristic.uuid}');
+          if (characteristic.uuid.toString() == notifyCharacteristicUUID) {
+            _notifyCharacteristic = characteristic;
+            print('Notify Characteristic Found: ${characteristic.uuid}');
+            await _notifyCharacteristic!.setNotifyValue(true);
+            _notifyCharacteristic!.value.listen((value) {
+              print('Received Value: $value');
+              if (value.isNotEmpty && value.length >= 4) {
+                // Parsing RPM data
+                int rpmValue = ((value[2] * 256) + value[3]) ~/ 4;
+                rpm.value = rpmValue;
+                print('Parsed RPM Value: $rpmValue');
+              } else {
+                print('Invalid RPM data received.');
+              }
+            });
+          }
         }
       }
-      sendRPMCommand();
-    }
-  }
-
-  void sendRPMCommand() async {
-    if (_writeCharacteristic != null) {
-      // Send the PID command for RPM
-      List<int> rpmCommand = [0x01, 0x0C];
-      await _writeCharacteristic!.write(rpmCommand, withoutResponse: true);
-    }
-  }
-
-  void sendRPMCommandManually() async {
-    if (_writeCharacteristic != null) {
-      // Send the PID command for RPM
-      List<int> rpmCommand = [0x01, 0x0C];
-      print("Sending RPM command: $rpmCommand");
-      await _writeCharacteristic!.write(rpmCommand, withoutResponse: false);
-
-      // Listen for response
-      await Future.delayed(Duration(seconds: 2)); // Adjust delay as needed
-      print("Received response: ${response.value}");
-    } else {
-      print("Write characteristic not found");
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,180 @@
+// import 'package:car_info/ble_controller.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'car info',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("scanner"),
+//       ),
+//       body: GetBuilder<BleController>(
+//         init: BleController(),
+//         builder: (BleController controller) {
+//           return Stack(
+//             children: [
+//               Column(
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: [
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   Obx(
+//                     () => Text(
+//                       "Connectto:${controller.connectedDevice.toString()}",
+//                     ),
+//                   ),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   Obx(
+//                     () => Text(
+//                       "service:${controller.service.length}",
+//                     ),
+//                   ),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   Obx(
+//                     () => Text(
+//                       "Rpm:${controller.rpm.toString()}",
+//                     ),
+//                   ),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   Obx(
+//                     () => Text(
+//                       controller.response.value,
+//                     ),
+//                   ),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       controller.sendRPMCommandManually();
+//                     },
+//                     child: const Text("send"),
+//                   ),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       controller.scanDevice();
+//                       showModalBottomSheet(
+//                         context: context,
+//                         builder: (context) {
+//                           return StreamBuilder(
+//                             stream: controller.scanResult,
+//                             builder: (context, snapshot) {
+//                               return Obx(() {
+//                                 if (controller.isScanning.value) {
+//                                   return const Center(
+//                                       child: CircularProgressIndicator());
+//                                 } else {
+//                                   if (snapshot.hasData) {
+//                                     if (snapshot.data!.isNotEmpty) {
+//                                       return ListView.builder(
+//                                         itemCount: snapshot.data!.length,
+//                                         itemBuilder: (context, index) {
+//                                           final data = snapshot.data![index];
+//                                           return GestureDetector(
+//                                             onTap: () {
+//                                               controller
+//                                                   .connectToDevice(data.device);
+//                                               if (Navigator.canPop(context)) {
+//                                                 Navigator.pop(context);
+//                                               }
+//                                             },
+//                                             child: ListTile(
+//                                               title: Text(
+//                                                 data.device.name.isEmpty
+//                                                     ? "Tanpa Nama"
+//                                                     : data.device.name,
+//                                               ),
+//                                               subtitle: Text(
+//                                                 data.device.id.id,
+//                                               ),
+//                                               trailing:
+//                                                   Text(data.rssi.toString()),
+//                                             ),
+//                                           );
+//                                         },
+//                                       );
+//                                     } else {
+//                                       if (controller.isScanning.value) {
+//                                         return const Center(
+//                                           child: CircularProgressIndicator(),
+//                                         );
+//                                       } else {
+//                                         return const Center(
+//                                           child: Text("tidak ditemukan device"),
+//                                         );
+//                                       }
+//                                     }
+//                                   } else {
+//                                     return const Center(
+//                                       child: Text("tidak ditemukan device"),
+//                                     );
+//                                   }
+//                                 }
+//                               });
+//                             },
+//                           );
+//                         },
+//                       );
+//                     },
+//                     child: const Text("Scan"),
+//                   ),
+//                 ],
+//               ),
+//               Obx(() {
+//                 if (controller.isConnecting.value) {
+//                   return const Center(
+//                     child: CircularProgressIndicator(),
+//                   );
+//                 }
+//                 return Container();
+//               }),
+//             ],
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
 import 'package:car_info/ble_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,16 +186,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'car info',
+      title: 'Car Info',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
@@ -35,117 +211,104 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("scanner"),
+        title: const Text("Scanner"),
       ),
       body: GetBuilder<BleController>(
         init: BleController(),
         builder: (BleController controller) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+          return Stack(
             children: [
-              Obx(
-                () => Text(
-                  "loading:${controller.isLoading.value.toString()}",
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Obx(
-                () => Text(
-                  "Connectto:${controller.connectedDevice.toString()}",
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Obx(
-                () => Text(
-                  "service:${controller.service.length}",
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Obx(
-                () => Text(
-                  "Rpm:${controller.rpm.toString()}",
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Obx(
-                () => Text(
-                  controller.response.value,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  controller.sendRPMCommandManually();
-                },
-                child: Text("send"),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  controller.scanDevice();
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (context) {
-                      return StreamBuilder(
-                        stream: controller.scanResult,
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-                            if (snapshot.data!.isNotEmpty) {
-                              return ListView.builder(
-                                itemCount: snapshot.data!.length,
-                                itemBuilder: (context, index) {
-                                  final data = snapshot.data![index];
-                                  return GestureDetector(
-                                    onTap: () {
-                                      controller.connectToDevice(data.device);
-                                    },
-                                    child: ListTile(
-                                      title: Text(
-                                        data.device.name,
-                                      ),
-                                      subtitle: Text(
-                                        data.device.id.id,
-                                      ),
-                                      trailing: Text(data.rssi.toString()),
-                                    ),
-                                  );
-                                },
-                              );
-                            } else {
-                              if (controller.isScanning.value) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              } else {
-                                return const Center(
-                                  child: Text("tidak ditemukan device"),
-                                );
-                              }
-                            }
-                          } else {
-                            return const Center(
-                              child: Text("tidak ditemukan device"),
-                            );
-                          }
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  Obx(
+                    () => Text(
+                      "Connected to: ${controller.connectedDevice.value?.name ?? 'Not connected'}",
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Obx(
+                    () => Text(
+                      "Service: ${controller.service.length}",
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Obx(
+                    () => Text(
+                      "RPM: ${controller.rpm.value}",
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Obx(
+                    () => Text(
+                      controller.response.value,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      controller.scanDevice();
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return StreamBuilder(
+                            stream: controller.scanResult,
+                            builder: (context, snapshot) {
+                              return Obx(() {
+                                if (controller.isScanning.value) {
+                                  return const Center(
+                                      child: CircularProgressIndicator());
+                                } else {
+                                  if (snapshot.hasData &&
+                                      snapshot.data!.isNotEmpty) {
+                                    return ListView.builder(
+                                      itemCount: snapshot.data!.length,
+                                      itemBuilder: (context, index) {
+                                        final data = snapshot.data![index];
+                                        return GestureDetector(
+                                          onTap: () {
+                                            controller
+                                                .connectToDevice(data.device);
+                                            if (Navigator.canPop(context)) {
+                                              Navigator.pop(context);
+                                            }
+                                          },
+                                          child: ListTile(
+                                            title: Text(
+                                              data.device.name.isEmpty
+                                                  ? "Tanpa Nama"
+                                                  : data.device.name,
+                                            ),
+                                            subtitle: Text(data.device.id.id),
+                                            trailing:
+                                                Text(data.rssi.toString()),
+                                          ),
+                                        );
+                                      },
+                                    );
+                                  } else {
+                                    return const Center(
+                                      child: Text("Tidak ditemukan device"),
+                                    );
+                                  }
+                                }
+                              });
+                            },
+                          );
                         },
                       );
                     },
-                  );
-                },
-                child: const Text("Scan"),
+                    child: const Text("Scan"),
+                  ),
+                ],
               ),
+              Obx(() {
+                if (controller.isConnecting.value) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return Container();
+              }),
             ],
           );
         },
